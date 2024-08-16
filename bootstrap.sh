5566c72b935caaef612ad087775798c199acae77
Alexnder Kentner <akentner@github.com>
2024-08-16 21:31:12 +0200

    Fixed: vars

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-USER="ansible"
-UID=1099
-USER_HOME="/home/${USER}"
-SSHDIR="${USER_HOME}/.ssh"
+ANSIBLE_USER="ansible"
+ANSIBLE_UID=1099
+ANSIBLE_HOME="/home/${ANSIBLE_USER}"
+SSHDIR="${ANSIBLE_HOME}/.ssh"
 SSHPUB="https://raw.githubusercontent.com/akentner/infrastructure_public/main/.ssh/ansible_management.pub"
 PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
 
-useradd -u $UID -s /bin/sh -U -p ${PASSWORD} $USER || true
+useradd -u $ANSIBLE_UID -s /bin/sh -U -p ${PASSWORD} $ANSIBLE_USER || true
 
 mkdir -p "$SSHDIR"
 chmod 700 "$SSHDIR"
 touch "$SSHDIR/authorized_keys"
 chmod 600 "$SSHDIR/authorized_keys"
-chown -R "$USER:$USER" $USER_HOME
+chown -R "$ANSIBLE_USER:$ANSIBLE_USER" $ANSIBLE_HOME
 
 curl $SSHPUB >> $SSHDIR/authorized_keys
 
 
-echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/$USER
+echo "$ANSIBLE_USER ALL=(ALL:ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/$ANSIBLE_USER
 
